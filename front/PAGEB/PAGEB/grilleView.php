12d16b5d42ec062bd01a5b90c7652bcd59dd9768
YohanLeHero <102321550+YohanLeHero@users.noreply.github.com>
2025-09-19 11:40:35 +0200

    Session

---
+++ front/PAGEB/PAGEB/grilleView.php
@@ -0,0 +1,39 @@
+<?php
+function afficherEtudiantAvecLiens($etudiant, $idUser, $role) {
+	session_start();
+	echo "<h2>Informations sur l'étudiant</h2>";
+	foreach ($etudiant as $etu) {
+		$_SESSION['idEtu'] = $etu['idEtu'];
+		if($etu['date_h'] > date("Y-m-d H:i:s")) {
+			echo "<h2>A Venir</h2>";
+			echo "<table border='1'>";
+			echo "<tr><th>Nom</th><td>{$etu['nom']}</td></tr>";
+			echo "<tr><th>Prénom</th><td>{$etu['prenom']}</td></tr>";
+			echo "<tr><th>Entreprise</th><td>{$etu['entreprise']}</td></tr>";
+			echo "<tr><th>Maitre Stage</th><td>{$etu['maitreStage']}</td></tr>";
+			echo "<tr><th>Sujet</th><td>{$etu['sujet']}</td></tr>";
+			echo "<tr><th>Date soutenance</th><td>{$etu['date_h']}</td></tr>";
+			echo "<tr><th>Salle</th><td>{$etu['salle']}</td></tr>";
+			echo "</table>";
+		}
+		else {
+			echo "<h2>Passer</h2>";
+			echo "<table border='1'>";
+			echo "<tr><th>Nom</th><td>{$etu['nom']}</td></tr>";
+			echo "<tr><th>Prénom</th><td>{$etu['prenom']}</td></tr>";
+			echo "<tr><th>Entreprise</th><td>{$etu['entreprise']}</td></tr>";
+			echo "<tr><th>Maitre Stage</th><td>{$etu['maitreStage']}</td></tr>";
+			echo "<tr><th>Sujet</th><td>{$etu['sujet']}</td></tr>";
+			echo "<tr><th>Date soutenance</th><td>{$etu['date_h']}</td></tr>";
+			echo "<tr><th>Salle</th><td>{$etu['salle']}</td></tr>";
+			echo "</table>";
+		}
+ 
+
+    echo "<h3>Actions disponibles pour $role</h3>";
+    echo "<ul>";
+    //echo "<li><a href='pageSuivante.php?action=portfolio&idUser=$idUser&idEtudiant={$etu['IdEtudiant']}'>Saisir/Consulter les grilles</a></li>";
+    echo "<li><a href='pageSuivante.php?<?php echo htmlspecialchars(SID); ?>'>Saisir/Consulter les grilles</a></li>"; 
+	echo "</ul>";
+	}
+}
